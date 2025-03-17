cb1a0a293ebdfb39256f0e7759682e822c17f75f
Osid ALkourd <alkordosid73@gmail.com>
2025-03-17 14:31:20 +0200

    add styling for JSX element

--- src/MyFirstComponent.js
+++ src/MyFirstComponent.js
@@ -1,10 +1,26 @@
 
 const number = 10;
-
+const styleProperty = {backgroundColor: "red" , fontSize: "25px"};
 function MyFirstComponent(){
     return(
-        <h1>{number + " "}this is my first component</h1>
+        <div>
+        <h1 style={styleProperty}>{number + " "}this is my first component</h1>
+        <button onClick={sayHello}>Button </button>
+        </div>
     )
+}
+// or replace the previous code with following 
+// function MyFirstComponent(){
+//     return(
+//         <div>
+//         <h1 style={{backgroundColor: "red" , fontSize: "25px"}}>{number + " "}this is my first component</h1>
+//         <button>Button </button>
+//         </div>
+//     )
+// }
+
+function sayHello(){
+    alert("Hello Guys");
 }
 
 export default MyFirstComponent;
