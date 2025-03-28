ca71e71949a8dc5d03a8318f4c2fc69e24b6fdac
Osid ALkourd <alkordosid73@gmail.com>
2025-03-28 12:43:58 +0200

    add select and readio input with state

--- src/MyForm.js
+++ src/MyForm.js
@@ -9,10 +9,22 @@ function MyForm() {
     email: "",
     age: "",
     isStudent: false,
+    gender: "male",
+    university: "",
   });
   function handleCheckbox(event) {
+    setFormInput({ ...formInputs, isStudent: event.target.checked });
+    console.log(event.target.value);
+  }
 
-       setFormInput({...formInputs, isStudent: event.target.checked})
+  function handleRadioButton(event) {
+    setFormInput({ ...formInputs, gender: event.target.value });
+    console.log(event.target.value);
+  }
+
+  function handleDropdownMenu(event) {
+    setFormInput({ ...formInputs, university: event.target.value });
+    console.log(event.target.value);
   }
   return (
     <div style={{ marginTop: "30px" }}>
@@ -45,7 +57,44 @@ function MyForm() {
         />
         <br />
         <label>is student</label>
-        <input type="checkbox" checked={formInputs.isStudent} onChange={handleCheckbox} />
+        <input
+          type="checkbox"
+          checked={formInputs.isStudent}
+          onChange={handleCheckbox}
+          value="osid2001"
+        />
+        <div>
+          <input
+            type="radio"
+            name="gender"
+            value="male"
+            checked={formInputs.gender === "male"}
+            onChange={handleRadioButton}
+          />
+          <label>male</label>
+          <input
+            type="radio"
+            name="gender"
+            value="female"
+            checked={formInputs.gender === "female"}
+            onChange={handleRadioButton}
+          />
+          <label>female</label>
+        </div>
+
+        <div>
+          <label for="university">university</label>
+          <select
+            id="university"
+            value={formInputs.university}
+            onChange={handleDropdownMenu}
+          >
+            <option value="">select a university</option>
+            <option value="islamic">islamic</option>
+            <option value="azhar">azhar</option>
+            <option value="aqsa">aqsa</option>
+          </select>
+        </div>
         <button>submit</button>
       </form>
     </div>
