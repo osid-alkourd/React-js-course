f5bfce4de971ba87d2cc65a69291bc149fdc4fc0
Osid ALkourd <alkordosid73@gmail.com>
2025-04-04 15:40:52 +0200

    Add Initial Value For Create Context

--- src/contexts/LoanFormInputContext.js
+++ src/contexts/LoanFormInputContext.js
@@ -1,3 +1,4 @@
 import { createContext } from "react";
-let  LoanInputContext = createContext();
+let  LoanInputContext = createContext({labelName: '' , handleChange: null , value: ''});
+// {labelName: '' , handleChange: null , value: ''} this is a initial context
 export {LoanInputContext};
